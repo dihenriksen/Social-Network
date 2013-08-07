d3d960cba7ca3ce8c64dd99576a3c5001d584c5e
Daniel Henriksen <danhenriksen12@gmail.com>
2013-08-07 16:07:10 -0700

    Add navbar to all pages

--- Gemfile
+++ Gemfile
@@ -2,9 +2,12 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
-
 gem 'atomic', '1.1.12'
 
+group :development, :test do
+	gem 'rspec-rails'
+	gem 'better_errors'
+end
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
@@ -13,7 +16,7 @@ gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
 
 # Use CoffeeScript for .js.coffee assets and views
-gem 'coffee-rails', '~> 4.0.0'
+# gem 'coffee-rails', '~> 4.0.0'
 
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Reputation::Application.routes.draw do
+  get "profile/new"
+  get "profile/create"
+  get "profile/show"
+  get "profile/edit"
+  get "profile/update"
+  get "profile/destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'profile#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -39,7 +45,7 @@ Reputation::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
