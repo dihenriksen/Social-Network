8e12e87f13272e9ab51a414305b95185ec4a3b81
Daniel Henriksen <danhenriksen12@gmail.com>
2013-08-17 16:52:10 -0700

    Profile page can alternate display between news and stories

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -3,9 +3,23 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(function() {
-	$(".select").click(function(e) {
+	$(".select").click(function() {
 		$(this).addClass('active');
 		$(".select").not($(this)).removeClass('active');
+
+		// Hide and Display appropriate comments
+		if ($(this).hasClass('select_news')) {
+			$(".visible")[0].style.visibility = 'hidden';
+			$(".visible").removeClass('visible');
+			$("#news").addClass('visible');
+			$("#news")[0].style.visibility = 'visible';
+		} else if ($(this).hasClass('select_stories')) {
+			console.log($(".visible")[0]);
+			$(".visible")[0].style.visibility = 'hidden';
+			$(".visible").removeClass('visible');
+			$("#stories").addClass('visible');
+			$("#stories")[0].style.visibility = 'visible';
+		}
 		return false
 	});
 });
